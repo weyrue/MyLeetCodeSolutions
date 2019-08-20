1ec389a44a7212b0b5f1625c92ee089eefc58c74
zy <weyrue@gmail.com>
2019-08-20 19:36:57 +0800

    25. Reverse Nodes in k-Group

--- src/solutions/Problem25ReverseNodesinkGroup/Solution.java
+++ src/solutions/Problem25ReverseNodesinkGroup/Solution.java
@@ -2,43 +2,32 @@ package solutions.Problem25ReverseNodesinkGroup;
 
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode pointer = head;
-        ListNode[] reverseGroup = new ListNode[k + 1];
-        /**
-         * 第一轮
-         */
-        for (int i = 1; i <= k; i++) {
-            if (pointer == null) return head;
-            reverseGroup[k - i] = pointer;
-            pointer = pointer.next;
-        }
-        reverseGroup[k] = pointer;
-        head = reverseGroup[0];
-        pointer = head;
-        for (int i = 0; i < k; i++) {
-            pointer.next = reverseGroup[i];
-            pointer = pointer.next;
-        }
-        if (reverseGroup[k] == null) return head;
-        pointer.next = reverseGroup[k];
-
+        if (k == 1) return head;
+        ListNode nodeBeforeHead = new ListNode(0), pointer = nodeBeforeHead;
+        nodeBeforeHead.next = head;
         while (true) {
-            ListNode pointer2 = pointer.next;
-            for (int i = 1; i <= k; i++) {
-                if (pointer2 == null) return head;
-                reverseGroup[k - i] = pointer2;
-                pointer2 = pointer2.next;
-            }
-            reverseGroup[k] = pointer2;
-
+            ListNode tmpPointer = pointer;
             for (int i = 0; i < k; i++) {
-                pointer.next = reverseGroup[i];
-                pointer = pointer.next;
+                if (tmpPointer.next == null) return nodeBeforeHead.next;
+                tmpPointer = tmpPointer.next;
             }
-            if (reverseGroup[k] == null) break;
-            pointer.next = reverseGroup[k];
+            pointer = reverseNodes(pointer, k);
+        }
+    }
+
+    private ListNode reverseNodes(ListNode nodeBeforeStartNode, int k) {
+        ListNode startNode = nodeBeforeStartNode.next, nodeBeforeReverseNode;
+        ListNode reverseNode = startNode;
+        ListNode nodeAfterReverseNode = startNode.next;
+        while (--k > 0) {
+            nodeBeforeReverseNode = reverseNode;
+            reverseNode = nodeAfterReverseNode;
+            nodeAfterReverseNode = nodeAfterReverseNode.next;
+            reverseNode.next = nodeBeforeReverseNode;
         }
 
-        return head;
+        nodeBeforeStartNode.next = reverseNode;
+        startNode.next = nodeAfterReverseNode;
+        return startNode;
     }
 }

ff48b4e480611c3fa35f6991713875016db2338a
zy <weyrue@gmail.com>
2019-11-05 17:17:20 +0800

    35. Search Insert Position

---
+++ src/solutions/Problem35SearchInsertPosition/MainClass.java
@@ -0,0 +1,31 @@
+package solutions.Problem35SearchInsertPosition;
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        String line = "[1,3,5,6]";
+        int[] nums = stringToIntegerArray(line);
+        int target = 5;
+
+        int ret = new Solution().searchInsert(nums, target);
+
+        String out = String.valueOf(ret);
+
+        System.out.print(out);
+    }
+}
---
+++ src/solutions/Problem35SearchInsertPosition/Solution.java
@@ -0,0 +1,8 @@
+package solutions.Problem35SearchInsertPosition;
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        return -1;
+    }
+}
